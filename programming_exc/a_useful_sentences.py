f1d355726e261e05f1668be90bbd14b31fa34f02
LesterDawn <452825100@qq.com>
2022-10-21 18:42:58 +0800

    compare the mean values of phyco between 2021 and 2022

--- programming_exc/a_useful_sentences.py
+++ programming_exc/a_useful_sentences.py
@@ -2,6 +2,10 @@
 1. 多列表同时排序
 """
 # end, start, sal = (list(t) for t in zip(*sorted(zip(end, start, sal))))
+"""
+2. 创建二维列表
+"""
+# record = [[''] * n2 for i in range(n1)]
 
 
 
---
+++ programming_exc/dp_LCS.py
@@ -0,0 +1,30 @@
+def LCS(s1: str, s2: str) -> str:
+    # write code here
+    res = ''
+    start, curr = 0, 0
+    n1, n2 = len(s1), len(s2)
+    s1, s2 = s1 if n1 >= n2 else s2, s2 if n1 >= n2 else s1
+    n1, n2 = len(s1), len(s2)
+    while True:
+        if curr == n1:
+            break
+
+        start = min(start, n2 - 1)
+        end = min(n1, curr + 1)
+        s2_curr = min(curr, n2 - 1)
+        if s1[curr] in s2[start:end]:
+            res += s1[curr]
+            start = curr
+        elif s2[s2_curr] in s1[start:curr + 1]:
+            res += s2[s2_curr]
+            start = curr
+        if s2[s2_curr] == s1[curr]:
+            start = curr + 1
+        curr += 1
+
+    return res if len(res) > 0 else -1
+
+s1 = "1a1a31"
+s2 = "1a231"
+
+print(LCS(s1, s2))

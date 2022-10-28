0ab92635a28d11bd5e6e05b5f801bebf0747c646
LesterDawn <452825100@qq.com>
2022-10-29 07:14:38 +0800

    online programming

--- programming_exc/dp_LCS.py
+++ programming_exc/dp_LCS.py
@@ -1,4 +1,4 @@
-def LCS(s1: str, s2: str) -> str:
+def LCS(s1: str, s2: str) -> str:  # 最长子序列
     # write code here
     res = ''
     n1, n2 = len(s1), len(s2)
@@ -16,6 +16,24 @@ def LCS(s1: str, s2: str) -> str:
             res = max(l, key=len)
     return res if res else '-1'
 
-s1 = "1a1a31"
-s2 = "1a231"
-print(LCS(s1, s2))
+
+def LCS1(str1: str, str2: str) -> str:  # 最长子串
+    # write code here
+    n1, n2 = len(str1), len(str2)
+    res = [[''] * (n2 + 1) for _ in range(n1 + 1)]
+    str1, str2 = ' ' + str1, ' ' + str2
+    for i in range(1, n1 + 1):
+        for j in range(1, n2 + 1):
+            if str2[j] == str1[i]:
+                res[i][j] = res[i - 1][j - 1] + str2[j]
+            else:
+                res[i][j] = ''
+    lcs = ''
+    for ele in res:
+        if len(max(ele, key=len)) > len(lcs):
+            lcs = max(ele, key=len)
+    return lcs
+
+
+s1, s2 = "abcdefg", "ab1cdefgabc1defg"
+print(LCS1(s1, s2))
